8aa867121c9d6732ca7290d613892f0fc9e31e28
deepakjohn1234 <deepakjohn032009@gmail.com>
2025-10-30 22:46:52 +0530

    Add files via upload

---
+++ myproject_2.cpp
@@ -0,0 +1,16 @@
+// first 10 natural numbers divisible by 7
+#include<iostream>
+#include<cstdio>
+using namespace std;
+in main()
+    {
+     int a=7;
+     cout<<"first 10 natural numbers divisible by 7 are";
+     while(a<=70)
+      {
+       cout<<a;
+       a=a+7;
+      }
+     return 0
+    }
+     
